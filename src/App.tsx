ef4fc4b267bee46b02be6ab8032c47febc5810b6
Hiroaki ITO <mail@isudzumi.com>
2022-09-19 17:45:48 +0900

    Implement html

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isAnnualPayment, setIfAnnualPayment] = useState(true)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="product">
+        <div className="description">
+          <h1>志空会 年会費</h1>
+          <h5>¥2000{isAnnualPayment ? ' / year' : ''}</h5>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <form method="POST">
+        <label htmlFor="annual_payment">継続支払いにする</label>
+        <input type="checkbox" id="annual_payment" onClick={() => setIfAnnualPayment(!isAnnualPayment)} />
+        <button id="checkout-and-portal-button" type="submit">
+          支払う
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </form>
     </div>
   )
 }
